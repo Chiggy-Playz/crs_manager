b9b2b4a4873fe8193ba64d843a15ccf5f0a189f3
Chiggy-Playz <garg.chirag2004@gmail.com>
2023-04-07 21:06:18 +0530

    Make buyer model equatable

--- lib/models/buyer.dart
+++ lib/models/buyer.dart
@@ -1,32 +1,37 @@
-class Buyer {
-    Buyer({
-        required this.name,
-        required this.address,
-        required this.gst,
-        required this.state,
-        required this.id,
-    });
+import 'package:equatable/equatable.dart';
 
-    String name;
-    String address;
-    String gst;
-    String state;
-    int id;
+class Buyer extends Equatable {
+  Buyer({
+    required this.name,
+    required this.address,
+    required this.gst,
+    required this.state,
+    required this.id,
+  });
 
-    factory Buyer.fromMap(Map<String, dynamic> json) => Buyer(
+  String name;
+  String address;
+  String gst;
+  String state;
+  int id;
+
+  @override
+  List<Object> get props => [name];
+
+  factory Buyer.fromMap(Map<String, dynamic> json) => Buyer(
         name: json["name"],
         address: json["address"],
         gst: json["gst"],
         state: json["state"],
         // id can be missing when the map is from challan model
         id: json["id"] ?? -1,
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "name": name,
         "address": address,
         "gst": gst,
         "state": state,
         "id": id,
-    };
+      };
 }
