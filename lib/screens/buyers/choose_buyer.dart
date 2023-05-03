a62e64fb3d0bb97e6d6e206ae1ca7c6c9cdf0d6c
Chiggy-Playz <garg.chirag2004@gmail.com>
2023-05-03 16:48:18 +0530

    Add `New buyer` button on buyer select page

--- lib/screens/buyers/choose_buyer.dart
+++ lib/screens/buyers/choose_buyer.dart
@@ -1,8 +1,10 @@
 import "package:crs_manager/providers/buyer_select.dart";
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
+import "package:responsive_sizer/responsive_sizer.dart";
 
 import "../../utils/widgets.dart";
+import "buyer_page.dart";
 import "buyers_list.dart";
 
 class ChooseBuyer extends StatelessWidget {
@@ -17,16 +19,31 @@ class ChooseBuyer extends StatelessWidget {
         return Scaffold(
           appBar: TransparentAppBar(
             title: const Text("Choose Buyer"),
+            
           ),
           body: const BuyersList(),
-          floatingActionButton: !selector.multiple
-              ? null
-              : FloatingActionButton(
+          floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              if (selector.multiple)
+                FloatingActionButton(
                   onPressed: () {
                     Navigator.of(context).pop(selector.selectedBuyers);
                   },
                   child: const Icon(Icons.check),
                 ),
+              SizedBox(height: 2.h),
+              FloatingActionButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const BuyerPage(),
+                  ));
+                },
+                child: const Icon(Icons.add),
+              ),
+              SizedBox(height: 2.h),
+            ],
+          ),
         );
       },
     );
